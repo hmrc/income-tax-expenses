2180949ead8e28cb47c99e8f94502295f15ff82f
Deng Nyuar <60218795+dnyuar@users.noreply.github.com>
2021-07-02 11:17:16 +0100

    Sass 1001 - Delete override employment expenses API  (#10)
    * connector added
    
    * [SASS-1001] it and unit tests added

---
+++ app/connectors/DeleteOverrideEmploymentExpensesConnector.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import config.AppConfig
+import connectors.httpParsers.DeleteOverrideEmploymentExpensesHttpParser.{DeleteOverrideEmploymentExpensesHttpReads, DeleteOverrideEmploymentExpensesResponse}
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import utils.DESTaxYearHelper.desTaxYearConverter
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class DeleteOverrideEmploymentExpensesConnector @Inject()(val http: HttpClient,
+                                                          val appConfig: AppConfig)(implicit val ec: ExecutionContext) extends DesConnector {
+
+  def deleteOverrideEmploymentExpenses(nino: String, taxYear: Int)(implicit hc: HeaderCarrier): Future[DeleteOverrideEmploymentExpensesResponse] = {
+
+    val incomeSourceUri: String = appConfig.desBaseUrl + s"/income-tax/expenses/employments/$nino/${desTaxYearConverter(taxYear)}"
+
+    def desCall(implicit hc: HeaderCarrier): Future[DeleteOverrideEmploymentExpensesResponse] = {
+      http.DELETE[DeleteOverrideEmploymentExpensesResponse](incomeSourceUri)
+    }
+
+    desCall(desHeaderCarrier(incomeSourceUri))
+
+  }
+
+}
---
+++ it/connectors/DeleteOverrideEmploymentExpensesConnectorSpec.scala
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package connectors
+
+import com.github.tomakehurst.wiremock.http.HttpHeader
+import config.AppConfig
+import helpers.WiremockSpec
+import models.{DesErrorBodyModel, DesErrorModel}
+import org.scalatestplus.play.PlaySpec
+import play.api.Configuration
+import play.api.http.Status._
+import uk.gov.hmrc.http.{HeaderCarrier, HeaderNames, HttpClient, SessionId}
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
+import utils.DESTaxYearHelper.desTaxYearConverter
+
+class DeleteOverrideEmploymentExpensesConnectorSpec extends PlaySpec with WiremockSpec {
+
+
+  lazy val connector: DeleteOverrideEmploymentExpensesConnector = app.injector.instanceOf[DeleteOverrideEmploymentExpensesConnector]
+  lazy val httpClient: HttpClient = app.injector.instanceOf[HttpClient]
+
+  def appConfig(desHost: String): AppConfig = new AppConfig(app.injector.instanceOf[Configuration], app.injector.instanceOf[ServicesConfig]) {
+    override val desBaseUrl: String = s"http://$desHost:$wireMockPort"
+  }
+
+  val nino = "123456789"
+  val taxYear = 2021
+
+
+  "DeleteOverrideEmploymentExpensesConnector" should {
+
+    val appConfigWithInternalHost = appConfig("localhost")
+    val appConfigWithExternalHost = appConfig("127.0.0.1")
+
+    val url = s"/income-tax/expenses/employments/$nino/${desTaxYearConverter(taxYear)}"
+
+    "include internal headers" when {
+
+      val headersSentToDes = Seq(
+        new HttpHeader(HeaderNames.authorisation, "Bearer secret"),
+        new HttpHeader(HeaderNames.xSessionId, "sessionIdValue")
+      )
+
+      "the host for DES is internal" in {
+
+        implicit val hc: HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId("sessionIdValue")))
+        val connector = new DeleteOverrideEmploymentExpensesConnector(httpClient, appConfigWithInternalHost)
+
+        stubDeleteWithoutResponseBody(url, NO_CONTENT, headersSentToDes)
+
+        val result = await(connector.deleteOverrideEmploymentExpenses(nino, taxYear)(hc))
+
+        result mustBe Right(())
+      }
+
+      "the host for DES is external" in {
+
+        implicit val hc:HeaderCarrier = HeaderCarrier(sessionId = Some(SessionId("sessionIdValue")))
+        val connector = new DeleteOverrideEmploymentExpensesConnector(httpClient, appConfigWithExternalHost)
+
+        stubDeleteWithoutResponseBody(url, NO_CONTENT, headersSentToDes)
+
+        val result = await(connector.deleteOverrideEmploymentExpenses(nino, taxYear)(hc))
+
+        result mustBe Right(())
+      }
+    }
+
+    "handle error" when {
+
+      val desErrorBodyModel = DesErrorBodyModel("DES_CODE", "DES_REASON")
+
+      Seq(INTERNAL_SERVER_ERROR, SERVICE_UNAVAILABLE, NOT_FOUND, BAD_REQUEST).foreach { status =>
+
+        s"Des returns $status" in {
+          val desError = DesErrorModel(status, desErrorBodyModel)
+          implicit val hc: HeaderCarrier = HeaderCarrier()
+
+          stubDeleteWithResponseBody(url, status, desError.toJson.toString())
+
+          val result = await(connector.deleteOverrideEmploymentExpenses(nino, taxYear)(hc))
+
+          result mustBe Left(desError)
+        }
+      }
+
+      "DES returns an unexpected error code - 502 BadGateway" in {
+        val desError = DesErrorModel(INTERNAL_SERVER_ERROR, desErrorBodyModel)
+        implicit val hc: HeaderCarrier = HeaderCarrier()
+
+        stubDeleteWithResponseBody(url, BAD_GATEWAY, desError.toJson.toString())
+
+        val result = await(connector.deleteOverrideEmploymentExpenses(nino, taxYear)(hc))
+
+        result mustBe Left(desError)
+      }
+
+    }
+
+
+  }
+}
